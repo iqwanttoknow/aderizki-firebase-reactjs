8d9acb3231f62fa8becbb5dd80745c20392fba3f
iqwanttoknow <71550214+iqwanttoknow@users.noreply.github.com>
2020-11-14 15:14:53 +0700

    Add files via upload

---
+++ src/components/coronaNews/index.js
@@ -0,0 +1,31 @@
+import React, { Fragment, useEffect, useState } from 'react';
+import app from '../../services/firebase';
+import 'firebase/database';
+
+const CoronaNews = () => {
+  const [news, setNews] = useState([]);
+  useEffect(() => {
+    const db = app.database().ref('news');
+    db.on('value', (snapshot) => {
+      const firebaseNews = snapshot.val();
+      setNews(firebaseNews.data);
+    });
+  }, []);
+
+  console.log('news', news);
+
+  return (
+    <div>
+      {news.map((item) => {
+ return (
+   <Fragment key={item.id}>
+     <p>{item.id}</p>
+     <p>{item.date}</p>
+   </Fragment>
+      );
+})}
+    </div>
+  );
+};
+
+export default CoronaNews;
